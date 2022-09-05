69a447d434cec8065a5e6c8752dced9cc3b27c13
coal691 <coleweber17@gmail.com>
2022-09-05 17:55:35 -0500

    Got the code written to play a round and it works. Next up will be a full game.

--- rock.js
+++ rock.js
@@ -15,4 +15,40 @@ function  getComputerChoice() {
 }
 const computerSelection = getComputerChoice();
 
-console.log(computerSelection)
+console.log(computerSelection);
+
+function getPlayerChoice() {
+    const pChoice = prompt("Rock, Paper, or Scissors?");
+    const playerChoice = pChoice.toLowerCase();
+    return playerChoice;
+};
+
+const playerSelection = getPlayerChoice();
+
+console.log(playerSelection);
+
+function round(computerSelection, playerSelection){
+    if (computerSelection=== "Rock" && playerSelection === "paper"){
+        return "You win! Paper beats Rock."
+    } else if (computerSelection === "Rock" && playerSelection === "rock"){
+        return "Wow! It's a tie!"
+    } else if (computerSelection === "Rock" && playerSelection === "scissors"){
+        return "Aw. You lost :("
+    }
+    if (computerSelection=== "Paper" && playerSelection === "scissors"){
+        return "You win! Scissors beats Paper."
+    } else if (computerSelection === "Paper" && playerSelection === "paper"){
+        return "Wow! It's a tie!"
+    } else if (computerSelection === "Paper" && playerSelection === "rock"){
+        return "Aw. You lost :("
+    }
+    if (computerSelection=== "Scissors" && playerSelection === "rock"){
+        return "You win! Rock beats Scissors."
+    } else if (computerSelection === "Scissors" && playerSelection === "scissors"){
+        return "Wow! It's a tie!"
+    } else if (computerSelection === "Scissors" && playerSelection === "paper"){
+        return "Aw. You lost :("
+    }
+}
+const rOutcome = round(computerSelection, playerSelection);
+console.log(rOutcome);
